a9be24c908587fc343bac9b0f798e2722cc4d035
Zellenon <zellenon@gmail.com>
2024-12-20 19:58:14 -0600

    updated to bevy 0.15, fixed example

--- examples/basic_methods.rs
+++ examples/basic_methods.rs
@@ -15,22 +15,22 @@ use bevy_composable::{
 };
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
-#[derive(Component, Default)]
+#[derive(Component, Default, Clone)]
 pub struct BodyPart;
 
-#[derive(Component, Default)]
+#[derive(Component, Default, Clone)]
 pub struct Torso;
 
-#[derive(Component, Default)]
+#[derive(Component, Default, Clone)]
 pub struct Arm;
 
-#[derive(Component, Default)]
+#[derive(Component, Default, Clone)]
 pub struct Leg;
 
-#[derive(Component, Default)]
+#[derive(Component, Default, Clone)]
 pub struct Hand;
 
-#[derive(Component, Default)]
+#[derive(Component, Default, Clone)]
 pub struct Foot;
 
 pub fn main() -> AppExit {
@@ -62,46 +62,44 @@ fn compose_demonstration(mut commands: Commands) {
 // Method 1 - The notation for declaring children causes linters to automatically format declared
 // trees in a shape that represents their actual layout.
 fn all_at_once() -> ComponentTree {
-    (Torso::store() + BodyPart::store() + name("Torso 1"))
-        << ((Arm::store() + BodyPart::store() + name("Arm"))
-            << (Hand::store() + BodyPart::store() + name("Hand")))
-        << ((Arm::store() + BodyPart::store() + name("Arm"))
-            << (Hand::store() + BodyPart::store() + name("Hand")))
-        << ((Leg::store() + BodyPart::store() + name("Leg"))
-            << (Foot::store() + BodyPart::store() + name("Foot")))
-        << ((Leg::store() + BodyPart::store() + name("Leg"))
-            << (Foot::store() + BodyPart::store() + name("Foot")))
+    (Torso.store() + BodyPart.store() + name("Torso 1"))
+        << ((Arm.store() + BodyPart.store() + name("Arm"))
+            << (Hand.store() + BodyPart.store() + name("Hand")))
+        << ((Arm.store() + BodyPart.store() + name("Arm"))
+            << (Hand.store() + BodyPart.store() + name("Hand")))
+        << ((Leg.store() + BodyPart.store() + name("Leg"))
+            << (Foot.store() + BodyPart.store() + name("Foot")))
+        << ((Leg.store() + BodyPart.store() + name("Leg"))
+            << (Foot.store() + BodyPart.store() + name("Foot")))
 }
 
 // Method 2 - For repeated parts, making functions that use other functions can make assembling
 // complex entities easy. I often use this approach for bullets and explosions.
 fn recursive() -> ComponentTree {
-    Torso::store() + BodyPart::store() + name("Torso 2") << arm() << arm() << leg() << leg()
+    Torso.store() + BodyPart.store() + name("Torso 2") << arm() << arm() << leg() << leg()
 }
 
 fn hand() -> ComponentTree {
-    Hand::store() + BodyPart::store() + name("Hand")
+    Hand.store() + BodyPart.store() + name("Hand")
 }
 
 fn arm() -> ComponentTree {
-    Arm::store() + BodyPart::store() + name("Arm") << hand()
+    Arm.store() + BodyPart.store() + name("Arm") << hand()
 }
 
 fn foot() -> ComponentTree {
-    Foot::store() + BodyPart::store() + name("Foot")
+    Foot.store() + BodyPart.store() + name("Foot")
 }
 
 fn leg() -> ComponentTree {
-    Leg::store() + BodyPart::store() + name("Leg") << foot()
+    Leg.store() + BodyPart.store() + name("Leg") << foot()
 }
 
 // Method 3 - note how you can use the same variable multiple times. This means you can store
 // component trees if you need/want to.
 fn variables() -> ComponentTree {
-    let bp = BodyPart::store();
-    let arm =
-        bp.clone() + name("Arm") + Arm::store() << (bp.clone() + name("Hand") + Hand::store());
-    let leg =
-        bp.clone() + Leg::store() + name("Leg") << (bp.clone() + name("Foot") + Foot::store());
-    Torso::store() + name("Torso 3") + bp << arm.clone() << arm << leg.clone() << leg
+    let bp = BodyPart.store();
+    let arm = bp.clone() + name("Arm") + Arm.store() << (bp.clone() + name("Hand") + Hand.store());
+    let leg = bp.clone() + Leg.store() + name("Leg") << (bp.clone() + name("Foot") + Foot.store());
+    Torso.store() + name("Torso 3") + bp << arm.clone() << arm << leg.clone() << leg
 }
